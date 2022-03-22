219bbcbec6e1f80a12d34c0e14727d98d871cfeb
GroovyHooked <dafrenchie2002@yahoo.fr>
2022-03-22 19:27:24 +0100

    Display result on page

--- action.js
+++ action.js
@@ -2,10 +2,13 @@ import { state } from "./store.js";
 import { BUY, SELL } from "./const.js";
 import { reducer } from "./reducer.js";
 
+const dataDisplay = document.querySelector("#state");
+
 export const buyItem = () => {
   const action = { type: BUY };
   reducer(state, action);
   console.log(state);
+  dataDisplay.innerHTML = state.item;
   return state;
 };
 
@@ -13,5 +16,6 @@ export const sellItem = () => {
   const action = { type: SELL };
   reducer(state, action);
   console.log(state);
+  dataDisplay.innerHTML = state.item;
   return state;
 };
